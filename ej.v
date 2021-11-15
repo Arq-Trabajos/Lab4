c5a02d73edffbcc5f052fcde148809eb320eac35
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-11-15 16:51:40 -0500

    inicial

---
+++ ej.v
@@ -0,0 +1,13 @@
+module ej (
+    input [1:0] ALUControl, input [31:0] A, input [31:0] B, input [31:0] Sum,
+    output [3:0] ALUFlags, output [31:0] result
+);
+
+endmodule
+
+module mux4_1(
+    input [1:0] S, input A, B, C, D,
+    output O
+);
+    
+endmodule
---
+++ ej_tb.v
@@ -0,0 +1,15 @@
+module ej_tb;
+
+    reg a, b;
+    wire c;
+
+    ej test(a,b,c);
+
+    initial begin
+        a = 1;
+        b = 2;
+        #1;
+        $display(a, b, c);
+    end
+
+endmodule
