a0f4c07f27750c4a4e7b3f311c6ed4688227f9e4
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-11-17 16:26:13 -0500

    avance de flags

--- alu.v
+++ alu.v
@@ -6,19 +6,25 @@ module alu (
     assign not_B = ~B;
 
     wire [31:0] mux2_1;
-    wire [31:0] adder;
+    wire [31:0] Sum;
     assign mux2_1 = (ALUControl[0]==0)? B: not_B;
-    assign adder = A+mux2_1+ALUControl[0];
+    assign Sum = A+mux2_1+ALUControl[0];
 
     wire [31:0] mux4_1;
     always @(*) begin
-        casex (ALUControl[1:0])
-            2b'0?: Result = adder;
+        casex (ALUControl)
+            2b'0?: Result = Sum;
             2b'10?: Result = A & B;
             2b'11?: Result = A | B;
         endcase
     end
 
+    wire Overflow, Carry,Negative, Zero;
+    assign Overflow = ~(ALUControl[0]^A^B) & (Sum[31:0]^A) & ~ALUControl[1];
+    assign Zero = 32b'0 == Result;
+    assign Negative = Result[31];
+    assign Carry = ~ALUControl[1] & Sum[32];
 
+    assign ALUFlags = {Negative, Zero, Carry, Overflow};    
 
 endmodule
